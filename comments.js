f66fa799bacc8a799f5f6ae8c765c23a602018df
Mauro <116788542+mauromltn@users.noreply.github.com>
2024-02-06 21:39:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,9 @@
 // Create web server
 const http = require('http');
-
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+    res.end('Hello, World!');
 });
 
 server.listen(3000, 'localhost', () => {
